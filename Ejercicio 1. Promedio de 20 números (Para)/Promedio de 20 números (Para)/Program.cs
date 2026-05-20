a598d319b240ecff928800d493490f900c142df8
ochelott <benjash8@outlook.com>
2026-05-19 22:09:25 -0300

    Ejercicio 1. Promedio de 20 numeros

---
+++ Ejercicio 1. Promedio de 20 números (Para)/Promedio de 20 números (Para)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio_de_20_números__Para_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, contador, acumulador, i;
+            double promedio;
+            acumulador = 0;
+            contador = 0;
+            for (i = 1; i <= 20; i++)
+            {
+                Console.WriteLine("Ingrese un número: ");
+                num = Convert.ToInt32(Console.ReadLine());
+               acumulador = acumulador + num;
+                contador = contador + 1;
+            }
+            promedio = (Double)acumulador / contador;
+            Console.WriteLine($"El promedio de los 20 números es: {promedio:F2}");
+        }
+    }
+}
---
+++ Ejercicio 1. Promedio de 20 números (Para)/Promedio de 20 números (Para)/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Promedio de 20 números (Para)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Promedio de 20 números (Para)")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("72e963d4-6978-4118-aba5-28c01a0639cd")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
