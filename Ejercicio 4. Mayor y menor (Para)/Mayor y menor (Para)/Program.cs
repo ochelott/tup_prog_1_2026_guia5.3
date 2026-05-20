604fe60946506a56d261dd5a97b340b3b7f6f72c
ochelott <benjash8@outlook.com>
2026-05-19 22:37:41 -0300

    Ejercicio 4 Mayor Menor (Para)

---
+++ Ejercicio 4. Mayor y menor (Para)/Mayor y menor (Para)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mayor_y_menor__Para_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, mayor, menor, i;
+            mayor = 0;
+            menor = 0;
+            for ( i = 1; i < 30; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                num = int.Parse(Console.ReadLine());
+                if (num > mayor)
+                {
+                    mayor = num;
+                }
+                if (num < menor)
+                {
+                    menor = num;
+                }
+            }
+            Console.WriteLine("El numero mayor es: " + mayor);
+            Console.WriteLine("El numero menor es: " + menor);
+
+            Console.ReadKey();
+        }
+    }
+}
